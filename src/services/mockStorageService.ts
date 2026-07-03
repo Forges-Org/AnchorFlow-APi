e9d484ad7657f6d2257deb05845c9bdb5be92df1
Nanafancy <133337507+Nanafancy@users.noreply.github.com>
2026-07-03 13:49:48 +0000

    service

--- src/services/mockStorageService.ts
+++ src/services/mockStorageService.ts
@@ -5,6 +5,9 @@
  */
 
 
+
+
+
 export const mockUploadToStorage = async (_file: Express.Multer.File): Promise<string> => {
   await new Promise(resolve => setTimeout(resolve, 1000));
   return `https://mock-storage.com/proof${Date.now()}.jpg`;
